22114cd7dd123254b7fba0407933c3006c966c43
Andrew Dev <aniq.dev@gmail.com>
2022-08-05 13:15:09 +0300

    made seeders

--- database/factories/PublisherFactory.php
+++ database/factories/PublisherFactory.php
@@ -17,7 +17,7 @@ class PublisherFactory extends Factory
     public function definition()
     {
         return [
-            //
+            'name' => fake()->unique()->city(),
         ];
     }
 }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use \App\Models\{Author, Publisher, Book, User};
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,11 +15,32 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        User::factory()->create([
+            'name' => 'Admin',
+            'email' => 'admin@admin.com',
+            'password' => bcrypt('password'),
+        ]);
+
+        User::factory(10)->create();
+
+        Author::factory(10)->create();
+
+        Publisher::factory(10)->create();
+
+        $publishers = Publisher::all()->pluck('id');
+
+        $authors = Publisher::all()->pluck('id');
+
+        for ($i=0; $i < 100; $i++) {
+            $book = Book::create([
+                'publisher_id' => $publishers->random(),
+                'title' => fake()->sentence,
+            ]);
+
+            $book->authors()->attach(
+                $authors->random(rand(1, 3))->toArray()
+            );
+        }
     }
 }
